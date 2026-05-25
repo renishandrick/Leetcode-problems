d28a524b2ae19154babebcfa51719d098ba30454
Renish Andrick <renishandrick@gmail.com>
2026-05-25 10:09:35 +0530

    Time: 23 ms (30.21%), Space: 59.4 MB (62.78%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -6,7 +6,7 @@ Queue<Integer>q;
     public int ping(int t) {
         q.offer(t);
         while(q.peek()<t-3000)
-        {
+        {    
             q.poll();
         }
         return q.size();
