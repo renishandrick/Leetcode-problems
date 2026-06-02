333e3eeba4f85d5566cfaab49819699ab8b4da8f
Renish Andrick <renishandrick@gmail.com>
2026-06-02 17:43:36 +0530

    Time: 12 ms (45.57%), Space: 46.8 MB (64.11%) - LeetHub

--- 0649-dota2-senate/0649-dota2-senate.java
+++ 0649-dota2-senate/0649-dota2-senate.java
@@ -1,26 +1,25 @@
 class Solution {
     public String predictPartyVictory(String senate) {
-        Queue <Integer> radient=new LinkedList<>();
+        Queue <Integer> rad=new LinkedList<>();
         Queue <Integer> dire=new LinkedList<>();
         int n=senate.length();
-        for(int i=0;i<senate.length();i++){
-            if(senate.charAt(i)=='R'){
-                radient.offer(i);
-            }
-            else{
-                dire.offer(i);
-            }
+        char ch[]=senate.toCharArray();
+        for(int i=0;i<ch.length;i++){
+            if(ch[i]=='R')
+            rad.offer(i);
+            else
+            dire.offer(i);
         }
-        while(!radient.isEmpty() && !dire.isEmpty()){
-            int r=radient.poll();
+        while(!rad.isEmpty()&& !dire.isEmpty()){
+            int r=rad.poll();
             int d=dire.poll();
             if(r<d){
-                radient.offer(r+n);
+                rad.offer(r+n);
             }
             else{
                 dire.offer(d+n);
             }
         }
-        return radient.isEmpty()?"Dire":"Radiant";
+        return rad.isEmpty() ? "Dire" : "Radiant";
     }
 }
