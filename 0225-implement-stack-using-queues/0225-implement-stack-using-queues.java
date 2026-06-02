4e5399e9f94e62457e6799c0c503261fed665c4f
Renish Andrick <renishandrick@gmail.com>
2026-06-02 19:59:03 +0530

    Time: 0 ms (100%), Space: 42.9 MB (7.85%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -1,30 +1,30 @@
 class MyStack {
-    Queue <Integer> q1;
-    Queue <Integer> q2;
+    Queue<Integer> q1;
+    Queue<Integer> q2;
     public MyStack() {
         q1=new LinkedList<>();
         q2=new LinkedList<>();
     }
+    
     public void push(int x) {
-        if(q1.isEmpty())
-        q1.offer(x);
-        else{
         while(!q1.isEmpty()){
-            q2.offer(q1.poll());
-        }
+          q2.offer(q1.poll());
+            }
         q1.offer(x);
         while(!q2.isEmpty()){
             q1.offer(q2.poll());
-         }
         }
+
     }
     
     public int pop() {
         return q1.poll();
+        
     }
     
     public int top() {
-       return q1.peek();
+        return q1.peek();
+        
     }
     
     public boolean empty() {
