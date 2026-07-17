8cf94dc0a348c4794c71410836788f8200bd08db
Renish Andrick <renishandrick@gmail.com>
2026-07-17 14:07:54 +0530

    Time: 0 ms (100%), Space: 43.7 MB (71.52%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -5,10 +5,9 @@ class Solution {
                 int temp=matrix[i][j];
                 matrix[i][j]=matrix[j][i];
                 matrix[j][i]=temp;
-                
             }
-        }
-        for(int i=0;i<matrix.length;i++){
+         }
+         for(int i=0;i<matrix.length;i++){
             int left=0;
             int right=matrix.length-1;
             while(left<right){
@@ -18,6 +17,6 @@ class Solution {
                 left++;
                 right--;
             }
-        }
+         }
     }
 }
